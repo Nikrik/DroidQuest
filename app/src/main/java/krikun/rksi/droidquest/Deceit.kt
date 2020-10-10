5fbcb8d08a3f83be98ef6efd02d453fff73d0fb2
Nikrik <krikunkrik@gmail.com>
2020-10-10 09:48:08 +0300

    bux fix

--- app/src/main/java/krikun/rksi/droidquest/Deceit.kt
+++ app/src/main/java/krikun/rksi/droidquest/Deceit.kt
@@ -4,29 +4,46 @@ import android.app.Activity
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class Deceit : AppCompatActivity() {
+
     private var Nikita: Button? = null
     private var question: TextView? = null
-    override fun onCreate(savedInstanceState: Bundle?) {
+
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_deceit)
         question = findViewById<View>(R.id.textView2) as TextView
         Nikita = findViewById<View>(R.id.button) as Button
         Nikita!!.setOnClickListener {
             setResult(Activity.RESULT_OK, Intent().putExtra("ispressed", true))
-            if (intent.getBooleanExtra(rksi, false)) {
+            if (intent.getBooleanExtra(rksi, false)){
                 question!!.setText(R.string.Yes)
             } else {
                 question!!.setText(R.string.No)
             }
-
-            //Toast.makeText(Deceit.this, R.string.cant_be_fooled,Toast.LENGTH_SHORT).show();
         }
+        if (savedInstanceState != null)
+        {
+            question!!.text = savedInstanceState.get("ispressed") as CharSequence?
+            if (question!!.text.isNotEmpty())
+            {
+                setResult(Activity.RESULT_OK, Intent().putExtra("ispressed", true))
+            }
+        }
+
+    }
+    public override fun onSaveInstanceState(outState: Bundle)
+    {
+        outState.putCharSequence("ispressed",question!!.text)
+        super.onSaveInstanceState(outState)
     }
 
     companion object {
